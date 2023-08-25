87bf39cc439b6830ed90ad0ffe7a86ad6752be45
Tiago da Silva Lourenco <tiagomutum@gmail.com>
2023-08-25 12:47:49 -0300

    chore: advancing in task

---
+++ cmd/desafio_multithreading/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/TiagoSilvaLourenco/desafio-multithreading/configs"
+	"github.com/TiagoSilvaLourenco/desafio-multithreading/infra/webserver/handlers"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	config, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Get("/", handlers.GetAddres)
+
+	http.ListenAndServe(config.WebServerPort, r)
+}

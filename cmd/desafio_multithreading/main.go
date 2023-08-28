f9d4d83ed55d344b6874faf3aae3d7ae16a50c0e
Tiago da Silva Lourenco <tiagomutum@gmail.com>
2023-08-28 16:06:19 -0300

    Task finished

--- cmd/desafio_multithreading/main.go
+++ cmd/desafio_multithreading/main.go
@@ -1,28 +1,71 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"sync"
+	"time"
 
-	"github.com/TiagoSilvaLourenco/desafio-multithreading/configs"
-	"github.com/TiagoSilvaLourenco/desafio-multithreading/internal/infra/webserver/handlers"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/TiagoSilvaLourenco/desafio-multithreading/internal/dto"
 )
 
-func main() {
-	config, err := configs.LoadConfig(".")
+const (
+	BrasilApi = "BrasilApiCep"
+	ViaCepApi = "ViaCepApi"
+)
+
+func request(url, nameApi string, wg *sync.WaitGroup) {
+
+	c := http.Client{Timeout: time.Microsecond}
+	req, err := c.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer req.Body.Close()
+	res, err := io.ReadAll(req.Body)
 	if err != nil {
 		panic(err)
 	}
 
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
+	var data interface{}
 
-	r.Route("/address", func(r chi.Router) {
-		r.Get("/{cep}", handlers.GetAddress) // This route should work
-	})
+	if nameApi == BrasilApi {
+		var brasilApiCep dto.BrasilApiCep
+		data = &brasilApiCep
+		data.(*dto.BrasilApiCep).NameApi = "Retornado por Brasil Cep API"
+	} else if nameApi == ViaCepApi {
+		var viaCep dto.ViaCep
+		data = &viaCep
+		data.(*dto.ViaCep).NameApi = "Retornado por ViaCep API"
+	} else {
+		fmt.Printf("API desconhecida")
+	}
 
-	http.ListenAndServe(config.WebServerPort, r)
+	err = json.Unmarshal(res, data)
+	if err != nil {
+		panic(err)
+
+	}
+	wg.Done()
+	log.Println(data)
+}
+
+func main() {
+	waitGroup := sync.WaitGroup{}
+	waitGroup.Add(1)
+
+	for _, cep := range os.Args[1:] {
+		urlViaCep := "http://viacep.com.br/ws/" + cep + "/json/"
+		urlBrasilCep := "https://brasilapi.com.br/api/cep/v1/" + cep
+
+		go request(urlBrasilCep, BrasilApi, &waitGroup)
+		go request(urlViaCep, ViaCepApi, &waitGroup)
+	}
+
+	waitGroup.Wait()
+
 }
